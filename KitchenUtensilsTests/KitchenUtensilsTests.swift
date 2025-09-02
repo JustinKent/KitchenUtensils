d54205b1c4977d2b09d088fb902b630c4ba0f3c0
Justin Kent <justin@justinkent.com>
2025-09-02 17:27:41 -0400

    Initial Xcode project creation

---
+++ KitchenUtensilsTests/KitchenUtensilsTests.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenUtensilsTests.swift
+//  KitchenUtensilsTests
+//
+//  Created by Justin Kent on 9/2/25.
+//
+
+import Testing
+@testable import KitchenUtensils
+
+struct KitchenUtensilsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
